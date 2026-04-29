8779a0c13a6f70fd2116d5e74219a09e30c36506
Jaouaher999 <salahjaoua2002@gmail.com>
2026-04-29 14:24:25 +0200

    chore: add sqlite3 as a dev dependency

--- db.js
+++ db.js
@@ -3,7 +3,9 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+export const sequelize = process.env.NODE_ENV === 'test'
+  ? new Sequelize('sqlite::memory:', { logging: false })
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'mysql',
